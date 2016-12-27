325778ee3e42d15fd89de67acb5ef881dae00e09
icodeweb <joyaljames@gmail.com>
2016-12-27 16:20:23 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TestEvent implements ShouldBroadcast
+{
+    public $text;
+
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    public function broadcastOn()
+    {
+        return ['test-channel'];
+    }
+}
+
+
+Route::get('/broadcast', function() {
+    event(new TestEvent('Broadcasting in Laravel using Pusher!'));
+
+    return view('welcome');
+});
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/bridge', function() {
+    $pusher = App::make('pusher');
+
+    $pusher->trigger( 'test-channel',
+                      'test-event', 
+                      array('text' => 'Preparing the Pusher Laracon.eu workshop!'));
+
+    return 'Executed pusher trigger function';
+});
